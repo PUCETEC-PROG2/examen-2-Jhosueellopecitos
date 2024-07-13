d35d50abdd404fce9fa413b16ac30c7e0f8f3304
PUCETEC <jilopezn@puce.edu.ec>
2024-07-13 10:25:18 -0500

    Programa Hecho

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+# Create your models here.
+
+class Movie (models.Model):
+    title= models.CharField(max_length=30, null=False)
+    genre = models.CharField(max_length=30, null=False)
+    director_name = models.CharField(max_length=30, null=False)
+    publication_year = models.IntegerField(null=False)
+    sipnosis = models.TextField (null=False)
+    
+    def __str__(self) -> str:
+        return self.title
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+
 
 # Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Movie
+
+def index(request):
+    movies = Movie.objects.order_by('genre')
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render({'movies:': movies}, request))
+
+def movie_details(request, movie_id):
+    movie = Movie.objects.get(mk = movie_id)
+    template = loader.get_template('display_movies.html')
+    context = {
+        'movie': movie
+    }
+    return HttpResponse(template.render(context, request))
